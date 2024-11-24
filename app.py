6f6fc5ef3b655bf2fed6d82d39bfe0c4884479f9
minettekota <me08ta24@gmail.com>
2024-11-25 01:33:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,3 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     #　デパッグもーっどをローカル環境では有効に、本番では無効に
     app.run(host='0.0.0.0', port=port, debug=False)
-    app.run(debug=True)
